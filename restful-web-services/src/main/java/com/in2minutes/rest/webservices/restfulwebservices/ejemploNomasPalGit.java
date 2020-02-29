d32e22f5838e2fcbc84d66137ca16774240dfd00
diego.alba <diego.alba@LSTKAG261007.SofttekClients.com>
2020-02-28 20:16:19 -0600

    se agrego la clase de git nomas por poner algo

---
+++ restful-web-services/src/main/java/com/in2minutes/rest/webservices/restfulwebservices/ejemploNomasPalGit.java
@@ -0,0 +1,10 @@
+package com.in2minutes.rest.webservices.restfulwebservices;
+
+public class ejemploNomasPalGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
